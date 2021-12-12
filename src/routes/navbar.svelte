60559114f9c3b5f430fc8a20aa4e89316eb9e9db
Luke Naylor <luke.naylr@gmail.com>
2021-12-12 01:38:06 +0000

    Refactor navbar

--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,8 +1,28 @@
 <script>
 	export let pagepath;
+
+	let nav_items = [
+		{
+			route: "/",
+			name: "Home"
+		},
+		{
+			route: "/blog",
+			name: "Blog"
+		},
+		{
+			route: "/contact",
+			name: "Contact"
+		}
+	];
+	
 </script>
 <nav class="nav nav-masthead justify-content-center float-md-end">
-	<a class="nav-link {pagepath==='/'?'active':''}" aria-current="page" href="/"> Home </a>
-	<a class="nav-link {pagepath==='/blog'?'active':''}" aria-current="page" href="/blog"> Blog </a>
-	<a class="nav-link {pagepath==='/contact'?'active':''}" aria-current="page" href="/contact"> Contact </a>
+	{#each nav_items as {route, name}}
+		<a class="nav-link {pagepath===route?'active':''}"
+		   aria-current="page"
+		   href="{route}">
+			{name}
+		</a>
+	{/each}
 </nav>
