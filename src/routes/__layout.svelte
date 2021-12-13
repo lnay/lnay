7cda5da41df7dcce62b85dc6b82e1a9b8fa0812c
Luke Naylor <luke.naylr@gmail.com>
2021-12-13 00:55:07 +0000

    Using scss extends to slim down the markup

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -17,19 +17,48 @@
 	<title>Lukidean Geometry</title>
 </svelte:head>
 
+<style lang="scss">
+	@use "../../node_modules/bootstrap/scss/bootstrap.scss";
+	div.outer {
+		@extend .d-flex;
+		@extend .h-100;
+		@extend .text-center;
+		@extend .text-white;
+		@extend .bg-dark;
+	}
+	div.inner {
+		@extend .d-flex;
+		@extend .w-100;
+		@extend .h-100;
+		@extend .p-3;
+		@extend .mx-auto;
+		@extend .flex-column;
+		transition: max-width 0.5s ease-out;
+	}
+	header {
+		@extend .mb-auto;
+	}
+	main {
+		@extend .px-3;
+	}
+	footer {
+		@extend .mt-auto;
+		@extend .text-white-50;
+	}
 
-<div class="d-flex h-100 text-center text-white bg-dark">
-	<div style="transition: max-width 2s;" class="{containerClass} d-flex w-100 h-100 p-3 mx-auto flex-column">
-		<header class="mb-auto">
-		<div>
+</style>
+
+
+<div class="outer">
+	<div class="{containerClass} inner">
+		<header>
 			<div class="float-md-start mb-0"><Logo extraClass="flat"/></div>
 			<Nav pagepath={pagepath}/>
-		</div>
 		</header>
 
-		<main class="px-3">
+		<main>
 			<h1>Cover your page.</h1>
-			<p class="lead">
+			<p>
 				Cover is a one-page template for building simple and beautiful home
 				pages. Download, edit the text, and add your own fullscreen background
 				photo to make it your own.
@@ -43,7 +72,7 @@
 			<slot></slot>
 		</main>
 
-		<footer class="mt-auto text-white-50">
+		<footer>
 		<p>
 			Cover template for
 			<a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>
