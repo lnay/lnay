a31f35b7b6690e5ba9f7f965a060ce07f016e5db
Luke Naylor <luke.naylr@gmail.com>
2022-06-29 00:23:00 +0100

    Add small reactive svg example

---
+++ src/routes/components/svg.svelte
@@ -0,0 +1,21 @@
+<script>
+	let r=40;
+	let rmax=50;
+</script>
+
+<p>
+<label>
+	r value:
+	<input type=range bind:value={r} min=0 max={rmax}>
+</label>
+<br/>
+<label>
+	svg size:
+	<input type=range bind:value={rmax} min=0 max=600>
+</label>
+</p>
+
+
+<svg height={2*rmax} width={2*rmax}>
+  <circle cx={rmax} cy={rmax} r={r} stroke="black" stroke-width="3" fill="red" />
+</svg>
