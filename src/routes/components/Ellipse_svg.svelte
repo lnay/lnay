6bdd04d37b6be819de5d0e5b358db239df2bc80b
Luke Naylor <luke.naylr@gmail.com>
2022-06-29 01:14:25 +0100

    Try svg experiment

---
+++ src/routes/components/Ellipse_svg.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Circ from "./Svg_ball.svelte"
+// elliptic curve given by y^2 = x((x-1)^2+a)
+	let width = 1900;
+	let height = 900;
+	let a = 0.02;
+	let b = 1;
+	let t = 0;
+	let scale = 800;
+	let left_offset=20;
+</script>
+
+<input type="range" bind:value={t} min="-10" max="10" step="0.01">
+t = {t}
+
+<svg {height} {width} style="border:solid black 2px">
+	<circle cx={left_offset + scale*(t*t)} cy={height/2 + scale*(t*Math.sqrt((t*t-1)**2+a))} r="40" stroke="black" stroke-width="3" fill="red" />
+	<Circ/>
+</svg>
---
+++ src/routes/components/Svg_ball.svelte
@@ -0,0 +1,1 @@
+<circle cx="100" cy="100" r="40" stroke="black" stroke-width="3" fill="purple" />
