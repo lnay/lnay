ae2daf6881e505514f9c5e1249432996270b5b0f
lnay <luke@grahamnaylor.net>
2022-07-26 00:19:09 +0100

    Add more points on the ellipse, demo moving point

--- src/routes/components/Ellipse_svg.svelte
+++ src/routes/components/Ellipse_svg.svelte
@@ -5,12 +5,17 @@
 	let height = 900;
 	let a = 0.02;
 	let b = 1;
-	let t = 0;
+	let time_var = 0;
+	$: t = 1.2 * Math.sin(time_var);
 	let scale = 800;
 	let left_offset=80;
 
 	let xpos = (t) => left_offset + scale*(t*t);
 	let ypos = (t) => height/2 + scale*(t*Math.sqrt((t*t-1)**2+a));
+
+	window.setInterval(() => {
+		time_var+=0.001;
+	}, 1);
 </script>
 
 <input type="range" bind:value={t} min="-10" max="10" step="0.01">
@@ -21,7 +26,7 @@ t = {t}
 	        cy={ypos(t)}
 	        r="40" stroke="black" stroke-width="3" fill="red" />
 	<Circ/>
-	{#each Array(25).fill().map((d, i) => i/10 + -1.2) as t0}
+	{#each Array(250).fill().map((d, i) => i/100 + -1.2) as t0}
 		<circle cx={xpos(t0)}
 		        cy={ypos(t0)}
 		        r="2" stroke="black" stroke-width="1" fill="purple" />
