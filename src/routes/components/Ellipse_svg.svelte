2c3f4b2bb24b900c7e1eed12212cbd39a302ad45
lnay <luke@grahamnaylor.net>
2022-07-26 00:07:26 +0100

    Add some dots along the elliptic curve

--- src/routes/components/Ellipse_svg.svelte
+++ src/routes/components/Ellipse_svg.svelte
@@ -7,13 +7,23 @@
 	let b = 1;
 	let t = 0;
 	let scale = 800;
-	let left_offset=20;
+	let left_offset=80;
+
+	let xpos = (t) => left_offset + scale*(t*t);
+	let ypos = (t) => height/2 + scale*(t*Math.sqrt((t*t-1)**2+a));
 </script>
 
 <input type="range" bind:value={t} min="-10" max="10" step="0.01">
 t = {t}
 
 <svg {height} {width} style="border:solid black 2px">
-	<circle cx={left_offset + scale*(t*t)} cy={height/2 + scale*(t*Math.sqrt((t*t-1)**2+a))} r="40" stroke="black" stroke-width="3" fill="red" />
+	<circle cx={xpos(t)}
+	        cy={ypos(t)}
+	        r="40" stroke="black" stroke-width="3" fill="red" />
 	<Circ/>
+	{#each Array(25).fill().map((d, i) => i/10 + -1.2) as t0}
+		<circle cx={xpos(t0)}
+		        cy={ypos(t0)}
+		        r="2" stroke="black" stroke-width="1" fill="purple" />
+	{/each}
 </svg>
