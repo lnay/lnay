e538a564b58b0f964d847590c862a83f563529c4
lnay <luke@grahamnaylor.net>
2023-07-16 01:51:05 +0100

    Tweak styles for overflow in teaching table

--- src/lib/Teaching.svelte
+++ src/lib/Teaching.svelte
@@ -8,13 +8,13 @@
         <td>
             <details>
                 <summary>{course.name}</summary>
-                <em>{course.level}</em>
+                <p style="text-align:right;font-style:italic;margin:0;font-size:0.9em;">course level: {course.level}</p>
                 <p>{course.description}</p>
             </details>
         </td>
         <td>
             <ul>
-            {#each course.tutoring as t}
+            {#each course.tutoring.sort().reverse() as t}
                 <li>{teaching_blocks[t.block].months} {t.Academic_year}</li>
             {/each}
             </ul>
@@ -39,13 +39,47 @@
         &:nth-child(2) {
             width: 13em;
             text-align: right;
-            color: red;
+            padding-bottom: 0;
+            position: relative;
+            &:after{
+                position: absolute;
+                content: "";
+                height: 1.6em;
+                width: 100%;
+                bottom: 0;
+                left: 0;
+                background: linear-gradient(transparent, #212529);
+            }
+            &:hover{
+                &:after {
+                    background: transparent;
+                }
+                & ul {
+                    height: auto; 
+                }
+            }
         }
-        & > ul {
+        ul {
             height: 1em;
             margin-bottom: 0;
-            line-height: 0.8;
+            line-height: 0.8em;
+            li {
+                list-style-type: none;
+                font-style: italic;
+            }
         }
+        details {
+            font-size: 0.8em;
+            summary {
+                font-size: 1.25em;
+            }
+            :not(summary) {
+                margin-left: 2em;
+            }
+        }
+    }
+    tr {
+        transition: height 0.5s;
     }
     * {
         font-style: normal;
