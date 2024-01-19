99275965df05ee09ae6c752cdc361fca71153990
lnay <luke@grahamnaylor.net>
2024-01-19 19:31:38 +0000

    Animate the navbar underline

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,4 +1,25 @@
 <script lang="ts">
+	import { crossfade } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+
+	let [send, receive] = crossfade({
+		duration: (d) => Math.sqrt(d * 200),
+
+		fallback(node, params) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: (t) => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+			};
+		}
+	});
+
 	import { page } from '$app/stores';
 	let pagepath: string;
 	$: pagepath = $page.url.pathname;
@@ -31,11 +52,18 @@
 	
 </script>
 <nav class="nav nav-masthead justify-content-center float-md-end">
-	{#each nav_items as {route, name}}
+	{#each nav_items as { route, name}}
 		<a class="nav-link {pagepath===route?'active':''}"
 		   aria-current="page"
 		   href="{route}">
 			{name}
+		{#if pagepath==route}
+		<div
+			class="underline"
+			in:receive={{ key: 1 , duration: 350}}
+			out:send={{ key: 1 , duration: 350}}
+		/>
+		{/if}
 		</a>
 	{/each}
 </nav>
@@ -46,12 +74,19 @@
 		font-weight: 700;
 		color: rgba(var(--foreground-color), .7);
 		background-color: transparent;
-		border-bottom: .25rem solid transparent;
+		border-bottom: none;
+		div.underline {
+			height: .25rem;
+			background-color: var(--foreground-accent-color-opaque);
+			width: 100%;
+		}
 	}
 
 	.nav-masthead .nav-link:hover,
 	.nav-masthead .nav-link:focus {
-		border-bottom-color: rgba(var(--foreground-accent-color), .25);
+		&:not(.active) {
+			color: var(--foreground-color);
+		}
 	}
 
 	.nav-masthead .nav-link + .nav-link {
@@ -60,6 +95,6 @@
 
 	.nav-masthead .active {
 		color: var(--foreground-color);
-		border-bottom-color: var(--foreground-accent-color-opaque);
+		border-bottom: none;
 	}
 </style>
