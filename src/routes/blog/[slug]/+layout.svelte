648858c00002c70f82f14be6de8bc3e505d9d8e7
lnay <luke@grahamnaylor.net>
2024-07-11 15:28:03 +0100

    Tweak spacing and sizing (with mostly blog in mind)

--- src/routes/blog/[slug]/+layout.svelte
+++ src/routes/blog/[slug]/+layout.svelte
@@ -2,24 +2,18 @@
 import { TableOfContents, tocCrawler } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="flex items-start relative xl:mx-10 place-content-evenly">
-<div id="page" class="w-full flex-auto max-w-4xl bottom-0">
+<div class="flex items-start relative xl:mx-10 place-content-evenly my-6">
 <main
-	id="page-content"
 	use:tocCrawler={{ mode: 'generate', scrollTarget: "#page" }}
+	id="page" class="w-full flex-auto max-w-4xl bottom-0 leading-relaxed text-xl"
 >
 <slot />
 </main>
-</div>
 <aside class="sticky top-10 hidden 2xl:block space-y-4 max-w-96 bottom-0 overflow-y-scroll">
 <TableOfContents />
+</aside>
 </div>
 
 <style lang="scss">
-main pre {
-    @apply pre;
-}
-main code {
-    @apply code;
-}
+
 </style>
